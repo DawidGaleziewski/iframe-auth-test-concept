5c2ca19bc44ef45eb1514fc2b10d7490683a6881
dawid <dawid.galeziewski@gmail.com>
2024-03-10 23:47:46 +0100

    update: was able to fetch token

--- src/components/IFrameChild.jsx
+++ src/components/IFrameChild.jsx
@@ -1,44 +1,48 @@
 import React, { useEffect, useState } from "react";
 export const IframeChild = () => {
-  const [recievedMessage, setRecievedMessage] = useState("");
-
-  // const sendMessage = () => {
-  //   window.parent.postMessage("Hi dad!", "http://localhost:3000");
-  // };
+  const sendMessage = (data) => {
+    window.parent.postMessage(data, "http://localhost:3000");
+  };
 
   useEffect(() => {
+    const handleIframeChildCommunication = (e) => {
+      if(!e.data || !e.data.type) return;
+      switch(e.data.type){
+        case 'redirect': {
+          // start redirect when reciving msg from spa
+          try {
+            sendMessage({type: 'start-fetch'})
+            window.location.href = e.data.payload;
+          } catch(e){
+            sendMessage({type: 'fail'})
+          }
+          break
+        }
+      }
+    }
     // when loading first time listener function is set. We want to recognize if we already have recived the token or not
     // const currentIframeUrl = window.location.href;
 
     // we want to know if we already have the required params or not:
     const urlParams = new URLSearchParams(window.location.search);
-    const myParam = urlParams.get('myParam');
-  
-    console.log('myParam', myParam, window.location);
+    const myParam = urlParams.get('token');
 
+    if(myParam){
+      console.log('success on reciving token', myParam)
+      sendMessage({type: 'success', payload: myParam})
+    }
+    // attempt 1 on error handling
+    // window.addEventListener('error', (e) => {
+    //   sendMessage({type: 'fail'})
+    // })
     // setup logic for communicating with main app
-    window.addEventListener("message", function (e) {
-      // if (e.origin !== "http://localhost:3000") return;
-      // handle all incoming msgs here
-      // setRecievedMessage("Got this message from parent: " + e.data);
-      console.log('recived in iframe',  e.data)
-
-      if(!e.data) return;
-
-      switch(e.data.type){
-        case 'redirect': {
-          window.location.href = e.data.payload
-        }
-      }
-    });
-  
+    window.addEventListener("message", handleIframeChildCommunication);
+    return () => window.removeEventListener("message", handleIframeChildCommunication)
   }, []);
 
   return (
     <div>
       <h2>Child iFrame</h2>
-      {/* <button onClick={sendMessage}>Send message to parent</button> */}
-      <p>{recievedMessage}</p>
     </div>
   );
 };
--- src/components/IFrameParent.jsx
+++ src/components/IFrameParent.jsx
@@ -1,33 +1,52 @@
 import React, { useRef, useEffect, useState } from "react";
 export const IframeParent = () => {
-    console.log('#iframe start')
   const iFrameRef = useRef(null);
-  const [recievedMessage, setRecievedMessage] = useState("");
+  const [token, setToken] = useState(null);
 
   // sending data to inframe
-  const sendMessage = () => {
+  const sendMessage = (data) => {
     if (!iFrameRef.current) return;
     iFrameRef.current.contentWindow.postMessage(
-      {type: 'redirect', payload: 'http://localhost:8080/first-chain-1'},
+      data,
       "http://localhost:3000"
     );
   };
 
-  // listening for data from iframe
+  // a hacky way to control error handling
+  //https://stackoverflow.com/questions/9249680/how-to-check-if-iframe-is-loaded-or-it-has-a-content
+
   useEffect(() => {
-    window.addEventListener("message", function (e) {
-      // if (e.origin !== "http://localhost:3000") return;
-      console.log('recived data from iframe: ', e.data)
-      setRecievedMessage("Got this message from child: " + e.data);
-    });
+    const handleIFrameParentCommunication = (e) => {
+      if(!e?.data?.type) return;
+      switch(e.data.type){
+        case 'start-fetch': {
+          console.log('started fetch from auth url')
+          break;
+        }
+        case 'success': {
+          console.log('success fetch of token: ',e.data, e.data.payload)
+          setToken(e.data.payload)
+          break;
+        }
+        case 'fail': {
+          console.log('crash on fetch')
+          break;
+        }
+      }
+    }
+    window.addEventListener("message", handleIFrameParentCommunication);
+
+    return () => window.removeEventListener("message", handleIFrameParentCommunication);
   }, []);
 
   return (
     <div>
       <h1>Parent iFrame</h1>
+      <h2>Recived token: {token}</h2>
       <button
         onClick={() => {
-          sendMessage();
+          // on click start redirect to desired endpint that will get us token back
+          sendMessage({type: 'redirect', payload: 'http://localhost:8080/first-chain-1'});
         }}
       >
         get access token
@@ -42,9 +61,19 @@ export const IframeParent = () => {
         width="600"
         height="300"
         title="Child iframe"
+        // attempt 2 on error handling
+        // onError={(e) => {
+        //   // handle errors in iframe here
+        //   console.log('fetch error occured')
+        //   window.parent.postMessage({type: 'fail'}, "http://localhost:3000");
+        // }}
+                // onError={(e) => {
+        //   // handle errors in iframe here
+        //   console.log('fetch error occured')
+        //   window.parent.postMessage({type: 'fail'}, "http://localhost:3000");
+        // }}
       ></iframe>
 
-      <p>{recievedMessage}</p>
     </div>
   );
 };
