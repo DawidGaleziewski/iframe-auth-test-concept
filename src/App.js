08318296e65e6097efd8e45f8bb7765bdc7d7903
dawid <dawid.galeziewski@gmail.com>
2024-03-10 19:19:17 +0100

    base window to parent communication via iframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Routes, createBrowserRouter,
+  RouterProvider, } from "react-router-dom";
+import { IframeParent } from "./components/IFrameParent";
+import { IframeChild } from "./components/IFrameChild";
+import { WindowParent } from "./components/WindowParent";
+import { WindowChild } from "./components/WindowChild";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/iframe/',
+    element:  <IframeParent />
+  },
+  {
+    path: '/iframe-child/',
+    element: <IframeChild />
+  },
+  {
+    path: '/window/',
+    element: <WindowParent />
+  }, 
+  {
+    path: "/window-child/",
+    element: <WindowChild />
+  }
+]);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <RouterProvider router={router} />
     </div>
   );
 }
